67bda152d393dafb3768c9ddaea75af4f8c91b37
Yugdeep Parihar <yugdeepparihar098@gmail.com>
2025-01-30 14:26:58 +0530

    Add project files.

---
+++ BridgeLabz_1/Demo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabz_1
+{
+    class Demo
+    {
+        static void Main(string[] arg) { 
+            Console.WriteLine("Hello");
+        }
+    }
+}
